41c282d6b2b8f9b431370fbeb92a155b559c329b
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2017-03-19 11:55:51 +0530

    SoC App V1.0 initial commit

---
+++ client.sh
@@ -0,0 +1,8 @@
+echo "R.G.D. Nayomal"
+echo "IT15027948"
+echo "Read more on https://rama41222.blogspot.com"
+echo "Starting up the client"
+echo "Press connect after entering a name"
+java -jar ./Executables/SoC_Chat_App_V_1.0_Client.jar &
+echo "Enjoy!!!"
+
---
+++ server.sh
@@ -0,0 +1,8 @@
+echo "R.G.D. Nayomal"
+echo "IT15027948"
+echo "Read more on https://rama41222.blogspot.com"
+echo "Starting up the server"
+echo "Press start"
+java -jar ./Executables/SoC_Chat_App_Server.jar &
+echo "Enjoy!!!"
+
